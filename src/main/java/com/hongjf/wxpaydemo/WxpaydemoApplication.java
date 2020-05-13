b0e7d63d626bc4c5835f86a82f48f9a3510343f6
hongjf <hongjf1996@163.com>
2020-05-13 20:01:43 +0800

    初始化上传

---
+++ src/main/java/com/hongjf/wxpaydemo/WxpaydemoApplication.java
@@ -0,0 +1,13 @@
+package com.hongjf.wxpaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxpaydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxpaydemoApplication.class, args);
+    }
+
+}
